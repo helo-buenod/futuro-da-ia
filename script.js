0347ca8ad86e095889d621139b8ca6aa8cf12fe7
helo-buenod <heloisabueno1508@gmail.com>
2025-08-28 17:13:25 -0300

    ..

--- script.js
+++ script.js
@@ -2,3 +2,24 @@ const caixaPergunta = document.querySelector(".caixa-pergunta");
 const caixaAlternativa = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+{
+enunciado: " onde a IA pode crescer muito no futuro?",
+alternativas: [
+"a) pintura"
+"b) agricultura"
+]
+},
+{ 
+    enunciado:"como garantir o uso seguro da IA?",
+    alternativas: [
+"a) sem regras"
+"b) ética e leis"
+
+    ]
+},
+{
+
+    
+
