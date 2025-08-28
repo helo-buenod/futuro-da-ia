2fdb57796a730321bcd9e0d19d5b4c3383f901ef
helo-buenod <heloisabueno1508@gmail.com>
2025-08-28 16:56:56 -0300

    ..

---
+++ script.js
@@ -0,0 +1,4 @@
+const caixaPergunta = document.querySelector(".caixa-pergunta");
+const caixaAlternativa = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
