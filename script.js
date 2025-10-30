fe6a26e412b9640011a07bdc4e769613b2630f94
helo-buenod <heloisabueno1508@gmail.com>
2025-10-30 16:28:28 -0300

    ..

--- script.js
+++ script.js
@@ -4,47 +4,74 @@ const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
-  { enunciado: "Onde a IA pode crescer muito no futuro?", alternativas: ["a) pintura", "b) agricultura"] },
-  { enunciado: "Como garantir o uso seguro da IA?", alternativas: ["a) sem regras", "b) ética e leis"] },
-  { enunciado: "O que a IA fará com os empregos?", alternativas: ["a) acabar com todos", "b) automatizar e criar novos"] },
-  { enunciado: "Qual o maior risco da IA?", alternativas: ["a) dominação total", "b) mau uso humano"] },
-  { enunciado: "Como a IA ajuda na saúde?", alternativas: ["a) substituir médicos", "b) diagnóstico precoce"] },
+{
+enunciado: " onde a IA pode crescer muito no futuro?",
+alternativas: [
+"a) pintura",
+"b) agricultura"
+]
+},
+{ 
+    enunciado:"como garantir o uso seguro da IA?",
+    alternativas: [
+"a) sem regras",
+"b) ética e leis"
+
+    ]
+},
+{
+    enunciado: "o que a IA fara com os empregos", 
+    alternativas: [ 
+        "a) acabar com todos",
+         "b) auromatizar e criar novos"
+    ]
+},
+  {
+    enunciado:"qual o maior risco da IA?",
+    alternativas:[
+        "a) acabar com todos",
+   "b) auromatizar e criar novos"
+    ]
+},
+{
+    enunciado: "como  a IA ajuda na saúde?",
+    alternativas: [
+    "a) substituir médicos",
+    "b) diagnostico cedo"
+]
+},
 ];
 
-let atual = 0;
+let atual= 0;
 let perguntaAtual;
-let historiaFinal = "";
+let historiaFinal ="";
 
-function mostraPergunta() {
-  perguntaAtual = perguntas[atual];
-  caixaPergunta.textContent = perguntaAtual.enunciado;
-  caixaAlternativa.textContent = "";
-  mostraAlternativa();
+function mostraPergunta () {
+    perguntaAtual = perguntas [atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
 }
 
 function mostraAlternativa() {
-  for (const alternativa of perguntaAtual.alternativas) {
-    const botaoAlternativa = document.createElement("button");
-    botaoAlternativa.textContent = alternativa;
-    botaoAlternativa.addEventListener("click", () => respostaSelecionada(alternativa));
-    caixaAlternativa.appendChild(botaoAlternativa);
-  }
+    for ( const alternativa of perguntaAtual.alternativas) {
+     const botaoAlternativas = document.createElement("button");
+     botaoAlternativas.textContent = alternativa;
+     caixaAlternativa.appendChild(botaoAlternativas);
+    }
 }
-
 function respostaSelecionada(opcaoSelecionada) {
-  historiaFinal += opcaoSelecionada + " ";
-  atual++;
-  if (atual < perguntas.length) {
-    mostraPergunta();
-  } else {
-    mostraResultado();
-  }
+    const afirmacoes = opcaoSelecionada.afirmacao;
+    historiafinal += afirmacoes + "";
+    atual++;
+mostraPergunta(); 
 }
 
-function mostraResultado() {
-  caixaPergunta.textContent = "Em 2049...";
-  textoResultado.textContent = historiaFinal;
-  caixaAlternativa.textContent = "";
+function mostraResultado () {
+    caixaPerguntas.textContent = "Em 2049...";
+    textoResultado.textContent = historiaFinal;
+    caixaAlternativas.textContent = "";
 }
 
-mostraPergunta();
+
+
+
+
